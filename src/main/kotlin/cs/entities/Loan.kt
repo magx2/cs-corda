6ad71b1ea0cb4951e76686067fab3c7a1e13087d
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-01-24 13:54:07 +0100

    Loan implements OwnableState

--- src/main/kotlin/cs/entities/Loan.kt
+++ src/main/kotlin/cs/entities/Loan.kt
@@ -1,10 +1,22 @@
 package cs.entities
 
 import com.google.common.base.Preconditions.checkArgument
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.OwnableState
+import net.corda.core.crypto.CompositeKey
 import java.math.BigDecimal
 import java.util.*
 
-class Loan(val total: BigDecimal, val interestRate: BigDecimal, val maturityDate: Date, val minimalInvest: BigDecimal, val expires: Date) {
+class Loan(val total: BigDecimal,
+           val interestRate: BigDecimal,
+           val maturityDate: Date,
+           val minimalInvest: BigDecimal,
+           val expires: Date,
+           override val owner: CompositeKey,
+           override val contract: Contract,
+           override val participants: List<CompositeKey>) : OwnableState{
+
     init {
         checkArgument(total.toDouble() > 0, "total == $total")
         checkArgument(interestRate.toDouble() > 0, "interestRate == $interestRate")
@@ -12,5 +24,9 @@ class Loan(val total: BigDecimal, val interestRate: BigDecimal, val maturityDate
         checkArgument(maturityDate > now, "maturityDate == ${maturityDate.time} > now == ${now.time}")
         checkArgument(minimalInvest.toDouble() > 0, "minimalInvest== $minimalInvest")
         checkArgument(expires > now, "expires == ${expires.time} > now == ${now.time}")
+    }
+
+    override fun withNewOwner(newOwner: CompositeKey): Pair<CommandData, OwnableState> {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
