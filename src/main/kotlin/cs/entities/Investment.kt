9f34d94618634fca846e77d2e9ed72343f3ed396
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-01-24 13:55:47 +0100

    Investment.kt implements OwnableState

--- src/main/kotlin/cs/entities/Investment.kt
+++ src/main/kotlin/cs/entities/Investment.kt
@@ -1,12 +1,23 @@
 package cs.entities
 
 import com.google.common.base.Preconditions.checkArgument
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.OwnableState
+import net.corda.core.crypto.CompositeKey
 import java.math.BigDecimal
 
-class Investment(val total: BigDecimal, val investor: Investor, val loan: Loan) {
+data class Investment(val total: BigDecimal,
+                      val investor: Investor,
+                      val loan: Loan,
+                      override val contract: Contract,
+                      override val owner: CompositeKey,
+                      override val participants: List<CompositeKey>) : OwnableState {
     init {
         checkArgument(total.toDouble() > 0.0, "total == $total")
         checkNotNull(investor)
         checkNotNull(loan)
     }
+
+    override fun withNewOwner(newOwner: CompositeKey): Pair<CommandData, OwnableState> = TODO()
 }
