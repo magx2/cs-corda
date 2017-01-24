f53833c3346d5025d61f57f0314824c500f70a44
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-01-24 13:56:03 +0100

    DateOfIssuing

--- src/main/kotlin/cs/entities/Investment.kt
+++ src/main/kotlin/cs/entities/Investment.kt
@@ -6,10 +6,12 @@ import net.corda.core.contracts.Contract
 import net.corda.core.contracts.OwnableState
 import net.corda.core.crypto.CompositeKey
 import java.math.BigDecimal
+import java.util.*
 
 data class Investment(val total: BigDecimal,
                       val investor: Investor,
                       val loan: Loan,
+                      val dateOfIssuing: Date = Date(),
                       override val contract: Contract,
                       override val owner: CompositeKey,
                       override val participants: List<CompositeKey>) : OwnableState {
